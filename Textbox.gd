ef9e4757974a4f5fcd44753cda4f1b0cf8fbcefa
Keaton Schwartz <keatonschwartz@berkeley.edu>
2022-10-15 22:50:43 -0700

    Add basic tutorial

--- Textbox.gd
+++ Textbox.gd
@@ -8,32 +8,40 @@ onready var end_symbol = $TextboxContainer/MarginContainer/HBoxContainer/End
 onready var label = $TextboxContainer/MarginContainer/HBoxContainer/Label2
 
 enum State {
+	TUTORIAL,
 	READY,
 	READING
 }
 
 var current_state = State.READY
 var text_queue = []
+var tutorial_text_queue = ["Move around with the arrow keys (Press Enter to Continue)", "Click the mouse to swing your sword (Press Enter to Continue)", "Good Luck!"]
 
 func _ready():
 	print("Starting state: State.READY")
-	hide_textbox()
-	queue_text("Saurabh? Are you there??")
-	queue_text("Abhi? are you there??")
-	queue_text("Is anyone there???")
+	display_text("Welcome to the Arena! (Press Enter to Continue)")
 
 
 func _process(delta):
 	match current_state:
+		State.TUTORIAL:
+			if !tutorial_text_queue.empty():
+				display_text(tutorial_text_queue.pop_front())
 		State.READY:
-			if !text_queue.empty():
-				display_text()
+			if !tutorial_text_queue.empty():
+				if Input.is_action_just_pressed("ui_accept"):
+					display_text(tutorial_text_queue.pop_front())
+			elif !text_queue.empty():
+				display_text(text_queue.pop_front())
 		State.READING:
 			if Input.is_action_just_pressed("ui_accept"):
 				label.percent_visible = 1.0
 				$Tween.remove_all()
 				end_symbol.text = "*"
-				change_state(State.READY)
+				if !tutorial_text_queue.empty():
+					change_state(State.TUTORIAL)
+				else: 
+					change_state(State.READY)
 
 
 func queue_text(next_text):
@@ -49,8 +57,8 @@ func show_textbox():
 	start_symbol.text = "*"
 	textbox_container.show()
 	
-func display_text():
-	var next_text = text_queue.pop_front()
+func display_text(next_text):
+	end_symbol.text = ""
 	label.text = next_text
 	label.percent_visible = 0.0
 	change_state(State.READING)

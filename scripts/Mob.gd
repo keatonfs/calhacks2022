abe1e62ca7c3f88a7db3cad36772ae6c37a58fbd
gundralaa <gundralaa@berkeley.edu>
2022-10-15 22:17:26 -0700

    mob spawner

--- scripts/Mob.gd
+++ scripts/Mob.gd
@@ -4,7 +4,7 @@ onready var _animated_sprite = $AnimatedSprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_animated_sprite.play("Idle")
+	_animated_sprite.play("Run")
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()

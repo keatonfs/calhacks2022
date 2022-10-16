a9e537c06c43b5f76616661a053a77a2fb80daa3
gundralaa <gundralaa@berkeley.edu>
2022-10-15 20:24:13 -0700

    mob instance

---
+++ scripts/Mob.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite.playing = true
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()

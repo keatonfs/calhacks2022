66dbff7a959b213c32d7dd736b6d792c5d37d897
saurabhnarain <saurabhnarain01@gmail.com>
2022-10-15 23:37:46 -0700

    add extra prompts

--- backend/prompts/player_dies.py
+++ backend/prompts/player_dies.py
@@ -20,6 +20,18 @@ Description: The monster chases Bob down. Bob tries to outrun the Monster but tr
 
 --
 
+Name: Keaton
+Enemy: Ghost
+Description: Keaton was trapped in a cave by the ghost. The ghost grabs Keaton's arm and holds it in place with its scythe. Keaton screams for help. He dies in the end.
+
+--
+
+Name: Maz
+Enemy: Ghoul
+Description: Maz was a maid at a high end house and did not have the tools to deal with a ghoul that was attacking the house. The ghoul takes Maz's face and rips it apart. Maz dies.
+
+--
+
 Name: {}
 Enemy: {}
 Description: 

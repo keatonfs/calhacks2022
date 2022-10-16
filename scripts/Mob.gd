0ab370e8d95aa559880a800eca7f699634b327e2
gundralaa <gundralaa@berkeley.edu>
2022-10-15 20:45:49 -0700

    mob scaling issues

--- scripts/Mob.gd
+++ scripts/Mob.gd
@@ -1,10 +1,10 @@
 extends RigidBody2D
 
+onready var _animated_sprite = $AnimatedSprite
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimatedSprite.playing = true
-	pass # Replace with function body.
-
+	_animated_sprite.play("Idle")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):

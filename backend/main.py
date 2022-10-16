41a9bc12d91903da0e52bfebca72c49126f5d4fa
saurabhnarain <saurabhnarain01@gmail.com>
2022-10-15 20:20:12 -0700

    add announcer prompt api call

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,31 @@
+from email.mime import base
 import cohere
+import prompts.introductions
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 
 co = cohere.Client("Cm65tYyCQf9sZEmKZY8oEkQCWG6Z8xmCEWhrTmOv")
 
+
+# USE: POST request to http://127.0.0.1:5000/announcer?$required-args
+# Example curl: curl -X POST "http://127.0.0.1:5000/announcer?name=Keaton&pronouns=he/him&descriptions=plain&record=None&nickname=keaty"
+class Announcer(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', required=True, location='args')
+        parser.add_argument('pronouns', required=True, location='args')
+        parser.add_argument('descriptions', required=True, location='args')
+        parser.add_argument('record', required=True, location='args')
+        parser.add_argument('nickname', required=True, location='args')
+
+        args = parser.parse_args()
+        base_prompt = prompts.introductions.prompt.format(args['name'], args['pronouns'], args['descriptions'], args['record'], args['nickname'])
+        response = co.generate(prompt = base_prompt, max_tokens=100).generations[0].text
+        return {'response': response}
+
+
 # USE: POST request to http://127.0.0.1:5000/generate?prompt=$prompt-goes-here
+# Example curl: curl -X POST "http://127.0.0.1:5000/generate?prompt=test"
 class BasicPromptGeneration(Resource):
     def post(self):
         parser = reqparse.RequestParser()
@@ -14,16 +35,20 @@ class BasicPromptGeneration(Resource):
         response = co.generate(prompt=args['prompt']).generations[0].text
         return {'response': response}, 200
 
+
 # USE: GET request to http://127.0.0.1:5000/running
+# Example curl: curl http://127.0.0.1:5000/running
 class IsRunning(Resource):
     def get(self):
         return {'running': 'true'}
 
+
 def main():
     app = Flask(__name__)
     api = Api(app)
-    api.add_resource(BasicPromptGeneration, '/generate')
     api.add_resource(IsRunning, '/running')
+    api.add_resource(BasicPromptGeneration, '/generate')
+    api.add_resource(Announcer, '/announcer')
     return app
 
 
---
+++ backend/prompts/introductions.py
@@ -0,0 +1,36 @@
+# Introductions
+# This text represents champion introductions into the ring during the tournament.The announcer will use information about the champion to construct a proper intro to the player.
+
+prompt = """
+Name: Abhi
+Pronouns: he/him
+Description: large, bulky, bald, unintelligent, angry
+Record: 100 wins 0 losses
+Nickname: Brute
+Announcer: Our undefeated champion, Abhi the brute will be entering the ring. The bald barbarian towers above his foes with a characteristic snarl. For all the muscle, our hero seems to be lacking the brains to enact his plans. Let us see how he fares against the hordes of enemies awaiting him.
+
+--
+
+Name: Jacky
+Pronouns: she/her
+Description: scrawny, fit, sneaky, deadly
+Record: 0 wins 0 losses
+Nickname: Blood Fox
+Announcer: We have a new contender in the ring today. From the harrowing deserts of the north, Jacky the Blood Fox is known for never missing a target. How will she dispatch the otherworldly beings in front of her? Lets hope we see blood tonight.
+
+--
+
+Name: Sam
+Pronouns: they/them
+Description: intelligent, strict, skilled, humble, kind
+Record: 0 wins 1 loss
+Nickname: Vengeful Knight
+Announcer: Today we witness the revenge of a great knight that was cheated out of a win. Sam the Vengeful Knight is known for their kind hearted demeanor and sword skill through the kingdom. Their heart is however corrupted by vengeance and craves for justice. With their superior wits and discipling, will Sam fulfill their agenda in the arena tonight?
+
+Name: {}
+Pronouns: {}
+Description: {}
+Record: {}
+Nickname: {}
+Announcer: 
+"""
